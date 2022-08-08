93022d1958cfabeef6ccf4682c369556647585ac
pau-riosa <jethro.riosa@gmail.com>
2022-08-08 14:35:37 +0800

    added an active tab

--- lib/todo_web/live/instructor/dashboard_live.ex
+++ lib/todo_web/live/instructor/dashboard_live.ex
@@ -16,7 +16,12 @@ defmodule TodoWeb.Instructor.DashboardLive do
       socket
       |> Schedules.assign_dates(params)
       |> Schedules.get_current_schedules(params)
+      |> handle_tab(params)
 
     {:noreply, socket}
+  end
+
+  def handle_tab(socket, params) do
+    assign(socket, :active_tab, params["class"])
   end
 end
